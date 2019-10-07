5b3087f043cde9d6fe70ab78755c4b8b7bfe974a
hereisfahad <fahadjabbar31@gmail.com>
2019-10-07 22:30:48 +0500

    solved margin gap between footer links

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="flexCenter mainWrapper">
       {/* {wrap h1 and nav in header t ag} */}
-      <h1 style={{paddingTop:'40px', marginBottom:'26px', fontWeight: '200'}} className="animated infinite heartBeat delay-2s">Fahad Jabbar</h1>
+      <h1 style={{paddingTop:'40px', marginBottom:'26px', fontWeight: '200'}} className=" animated infinite heartBeat delay-2s">Fahad Jabbar</h1>
       <Navbar />
       
       <Route path="/" component={Home} exact />
@@ -28,20 +28,21 @@ function App() {
       <Route path="/about" component={About} exact />
 
       <footer >
-        <a href="https://github.com/hereisfahad" rel="noopener noreferrer" target="_blank">
-          <Github style={{width:30, height:30, marginRight:'16px', marginBottom:'10px'}} />
+        <a href="https://github.com/hereisfahad" rel="noopener noreferrer" target="_blank"  >
+          <Github style={{
+            width:30, height:30, marginBottom:'10px'}} />
         </a>
-        <a href="https://www.linkedin.com/in/mrfahadjabbar/" rel="noopener noreferrer" target="_blank">
-          <LinkedIn style={{width:30, height:30, marginRight:'16px', marginBottom:'10px'}} />
+        <a href="https://www.linkedin.com/in/mrfahadjabbar/" rel="noopener noreferrer" target="_blank" >
+          <LinkedIn style={{width:30, height:30, marginBottom:'10px'}} />
         </a>  
-        <a href="https://www.twitter.com/hereisfahad" rel="noopener noreferrer" target="_blank">
-          <Twitter style={{width:30, height:30, marginRight:'16px', marginBottom:'10px'}} />
+        <a href="https://www.twitter.com/hereisfahad" rel="noopener noreferrer" target="_blank" >
+          <Twitter style={{width:30, height:30, marginBottom:'10px'}} />
         </a> 
-        <a href="https://www.facebook.com/hereisfahad" rel="noopener noreferrer" target="_blank">
-          <Facebook style={{width:30, height:30, marginRight:'16px', marginBottom:'10px'}} />
+        <a href="https://www.facebook.com/hereisfahad" rel="noopener noreferrer" target="_blank" >
+          <Facebook style={{width:30, height:30,  marginBottom:'10px'}} />
         </a> 
-        <a href="https://wa.me/+923009199076?text=Hey%20Fahad%20are%20you%20available%20to%20talk%20." rel="noopener noreferrer" target="_blank">
-          <Whatsapp style={{width:30, height:30, marginRight:'16px', marginBottom:'10px'} }/>
+        <a href="https://wa.me/+923009199076?text=Hey%20Fahad%20are%20you%20available%20to%20talk%20." rel="noopener noreferrer" target="_blank" >
+          <Whatsapp style={{width:30, height:30,  marginBottom:'10px'} }/>
         </a> 
       </footer>
     </div>
