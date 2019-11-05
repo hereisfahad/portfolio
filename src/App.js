4229f9d37ee10b9fff9575ecf44a3f8491384557
hereisfahad <fahadjabbar31@gmail.com>
2019-11-06 03:25:36 +0500

    added fa icons

--- src/App.js
+++ src/App.js
@@ -1,49 +1,102 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Navbar from './components/navbar/navbar.component';
-import Home from './components/Home/home.component';
-import Projects from './components/projects/projects.component';
-import Contact from './components/contact/contact.component';
-import About from './components/about/about.component';
+import Navbar from "./components/navbar/navbar.component";
+import Home from "./components/Home/home.component";
+import Projects from "./components/projects/projects.component";
+import Contact from "./components/contact/contact.component";
+import About from "./components/about/about.component";
 
-import { Route } from 'react-router-dom';
-
-import {  ReactComponent as Facebook } from './components/contact/facebook.svg';
-import {  ReactComponent as Twitter } from './components/contact/twitter.svg';
-import {  ReactComponent as Whatsapp } from './components/contact/whatsapp.svg';
-import {  ReactComponent as Github } from './components/contact/github.svg';
-import {  ReactComponent as LinkedIn } from './components/contact/linkedin.svg';
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="flexCenter mainWrapper">
       {/* {wrap h1 and nav in header t ag} */}
-      <h1 style={{paddingTop:'40px', marginBottom:'26px', fontWeight: '200'}} className=" animated infinite heartBeat delay-2s ">Fahad Jabbar</h1>
+      <h1
+        style={{ paddingTop: "40px", marginBottom: "26px", fontWeight: "200" }}
+        className=" animated infinite heartBeat delay-2s "
+      >
+        Fahad Jabbar
+      </h1>
       <Navbar />
-      
+
       <Route path="/" component={Home} exact />
       <Route path="/projects" component={Projects} exact />
       <Route path="/contact" component={Contact} exact />
       <Route path="/about" component={About} exact />
 
-      <footer >
-        <a href="https://github.com/hereisfahad" rel="noopener noreferrer" target="_blank"  >
-          <Github style={{
-            width:30, height:30, marginBottom:'10px'}} className="grow"/>
+      <footer>
+        <a
+          href="https://github.com/hereisfahad"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i
+            class="fab fa-github grow"
+            style={{
+              marginBottom: "10px",
+              color: "black",
+              fontSize: "30px"
+            }}
+          ></i>
         </a>
-        <a href="https://www.linkedin.com/in/mrfahadjabbar/" rel="noopener noreferrer" target="_blank" >
-          <LinkedIn style={{width:30, height:30, marginBottom:'10px'}} className="grow" />
-        </a>  
-        <a href="https://www.twitter.com/hereisfahad" rel="noopener noreferrer" target="_blank" >
-          <Twitter style={{width:30, height:30, marginBottom:'10px'}} className="grow"/>
-        </a> 
-        <a href="https://www.facebook.com/hereisfahad" rel="noopener noreferrer" target="_blank" >
-          <Facebook style={{width:30, height:30,  marginBottom:'10px'}} className="grow"/>
-        </a> 
-        <a href="https://wa.me/+923009199076?text=Hey%20Fahad%20are%20you%20available%20to%20talk%20." rel="noopener noreferrer" target="_blank" >
-          <Whatsapp style={{width:30, height:30,  marginBottom:'10px'}} className="grow"/>
-        </a> 
+        <a
+          href="https://www.linkedin.com/in/mrfahadjabbar/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i
+            class="fab fa-linkedin grow"
+            style={{
+              marginBottom: "10px",
+              color: "black",
+              fontSize: "30px"
+            }}
+          ></i>
+        </a>
+        <a
+          href="https://www.twitter.com/hereisfahad"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i
+            class="fab fa-twitter grow"
+            style={{
+              marginBottom: "10px",
+              color: "black",
+              fontSize: "30px"
+            }}
+          ></i>
+        </a>
+        <a
+          href="https://www.facebook.com/hereisfahad"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i
+            class="fab fa-facebook grow"
+            style={{
+              marginBottom: "10px",
+              color: "black",
+              fontSize: "30px"
+            }}
+          ></i>
+        </a>
+        <a
+          href="https://wa.me/+923009199076?text=Hey%20Fahad%20are%20you%20available%20to%20talk%20."
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <i
+            class="fab fa-whatsapp grow"
+            style={{
+              marginBottom: "10px",
+              color: "black",
+              fontSize: "30px"
+            }}
+          ></i>
+        </a>
       </footer>
     </div>
   );
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import './navbar.styles.css';
+import React from "react";
+import "./navbar.styles.css";
 
-import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     // <div className="">
-        <Nav defaultActiveKey="/home" className="navbarStyle" >
-          <Link style={{textDecoration:'none'}} className="link " to='/'> Home</Link>
-          <Link style={{textDecoration:'none'}}  className="link" to='/projects' >Projects</Link>
-          <Link style={{textDecoration:'none'}}  className="link " to='/contact' >Contact</Link>
-          <Link style={{textDecoration:'none'}}  className="link " to='/about' >About</Link>
-        </Nav>
+    <Nav defaultActiveKey="/home" className="navbarStyle">
+      <div id="bubble"></div>
+      <Link style={{ textDecoration: "none" }} className="link " to="/">
+        {" "}
+        Home
+      </Link>
+      <Link style={{ textDecoration: "none" }} className="link" to="/projects">
+        Projects
+      </Link>
+      <Link style={{ textDecoration: "none" }} className="link " to="/contact">
+        Contact
+      </Link>
+      <Link style={{ textDecoration: "none" }} className="link " to="/about">
+        About
+      </Link>
+    </Nav>
     // </div>
-    
-  )}
+  );
+}
 
 export default Navbar;
