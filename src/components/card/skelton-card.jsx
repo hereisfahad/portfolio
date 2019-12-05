de01893f257e68669d5923fb4ee7c6cd7331f795
hereisfahad <fahadjabbar31@gmail.com>
2019-12-06 02:51:04 +0500

    tweaking placeholder card style

--- src/components/card/skelton-card.jsx
+++ src/components/card/skelton-card.jsx
@@ -4,46 +4,20 @@ import "./skelton.css";
 import { Card } from "react-bootstrap";
 
 function SkeltonCard() {
-  const iconStyles = {
-    backgroundColor: "white",
-    width: "35px",
-    height: "35px",
-    borderRadius: "50%"
-  };
-  const flex = {
-    marginTop: "30px",
-    display: "flex",
-    justifyContent: "space-evenly"
-  };
-  const line = {
-    height: "10px",
-    width: "90%",
-    backgroundColor: "white",
-    margin: "5px auto",
-    marginBottom: "10px"
-  };
-  const title = {
-    height: "25px",
-    width: "100px",
-    backgroundColor: "white",
-    margin: "0 auto",
-    marginBottom: "18px"
-  };
   return (
-    <Card className="cardy">
-      <div
-        variant="top"
-        src=""
-        style={{ height: "100px", backgroundColor: "white", margin: "5px" }}
-      />
+    <Card className="cardy loading">
+      <div variant="top" src="" className="imgPlace" />
       <Card.Body>
-        <Card.Title style={title}></Card.Title>
-        <Card.Subtitle style={line}></Card.Subtitle>
-        <Card.Subtitle style={line}></Card.Subtitle>
-        <Card.Subtitle style={line}></Card.Subtitle>
-        <div style={flex}>
-          <div style={iconStyles}></div>
-          <div style={iconStyles}></div>
+        <Card.Title className="title"></Card.Title>
+        <br></br>
+        <Card.Subtitle className="line"></Card.Subtitle>
+        <br></br>
+        <Card.Subtitle className="line"></Card.Subtitle>
+        <br></br>
+        <Card.Subtitle className="line"></Card.Subtitle>
+        <div className="iconParent">
+          <div className="iconStyles"></div>
+          <div className="iconStyles"></div>
         </div>
       </Card.Body>
     </Card>
