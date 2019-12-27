b17cdc9432d3952ea045ab18e5e71c9350873129
hereisfahad <fahadjabbar31@gmail.com>
2019-12-27 21:23:04 +0500

    netlify react controlled form

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,41 +1,88 @@
 import React from "react";
 
 import "./contact.styles.css";
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
-function Contact() {
-  const h2Styles = {
-    color: "#9a9a3ec9",
-    marginBottom: "1.5rem"
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
   };
-  return (
-    <div className="showCaseDiv animated lightSpeedIn textDiv">
-      <h2 style={h2Styles}>Don't be a stranger</h2>
-      <p style={{ color: "#3e3e36ed", marginBottom: "26px" }}>
-        Feel free to get in touch with me. I am always open to discussing new
-        projects, creative ideas or opportunities to be a part of your visions.
-      </p>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <div className="showCaseDiv animated lightSpeedIn textDiv">
+        <h2
+          style={{
+            color: "#9a9a3ec9",
+            marginBottom: "1.5rem"
+          }}
+        >
+          Don't be a stranger
+        </h2>
+        <p style={{ color: "#3e3e36ed", marginBottom: "26px" }}>
+          Feel free to get in touch with me. I am always open to discussing new
+          projects, creative ideas or opportunities to be a part of your
+          visions.
         </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </div>
-  );
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>
+              Your Name:{" "}
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email:{" "}
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:{" "}
+              <textarea
+                name="message"
+                value={message}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Contact;
